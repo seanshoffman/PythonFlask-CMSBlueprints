a1b0ea309b2c4c685a5b2352ef86a714b287eb6a
Tom Bell <tjbell@gmail.com>
2019-09-16 16:07:07 -0600

    refactor: admin blueprint

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for, abort
 
-from cms.admin.models import db, Content, Setting, Type, User
+from cms.admin import admin_bp
+from cms.admin.models import db, Content, Type
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/{}'.format(app.root_path, 'content.db')
@@ -21,36 +22,7 @@ def index(slug):
     content = Content.query.filter(Content.slug == slug).first_or_404()
     return render_template('index.html', titles=titles, content=content)
 
-def requested_type(type):
-    types = [row.name for row in Type.query.all()]
-    return True if type in types else False
-
-@app.route('/admin', defaults = {'type': 'page'})
-@app.route('/admin/<type>')
-def content(type):
-    if requested_type(type):
-        content = Content.query.join(Type).filter(Type.name == type)
-        return render_template('admin/content.html', type=type, content=content)
-    else:
-        abort(404)
-
-@app.route('/admin/create/<type>')
-def create(type):
-    if requested_type(type):
-        types = Type.query.all()
-        return render_template('admin/content_form.html', title='Create', types=types, type_name=type)
-    else:
-        abort(404)
-
-@app.route('/admin/users')
-def users():
-    users = User.query.all()
-    return render_template('admin/users.html', title='Users', users=users)
-
-@app.route('/admin/settings')
-def settings():
-    settings = Setting.query.all()
-    return render_template('admin/settings.html', title='Settings', settings=settings)
+app.register_blueprint(admin_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
